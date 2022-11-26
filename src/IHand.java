d15b26ec3ec1567508b198e221993e4dd42239a3
knedlicka <60717210+knedlicka@users.noreply.github.com>
2022-11-26 16:44:36 +0100

    Add interfaces for Hand and DrawingAndTrashPile (#6)

---
+++ src/IHand.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public interface IHand {
+    public Optional<List<Card>> pickCards(List<HandPosition> positions);
+    public Map<HandPosition, Card> removePickedCardsAndRedraw();
+    public List<Card> returnPickedCards();
+    public boolean hasCardOfType(CardType type);
+    public List<Card> getCards();
+}

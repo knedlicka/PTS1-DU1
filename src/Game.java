53e77d46edf62fba66c8fb19a78b045c85c3cac8
Nadiya Balanchuk <balanchuknadiy4@gmail.com>
2022-11-30 21:41:14 +0100

    Add constructor in Game, WIP
    * Change constructor in SleepingQueens
      for better distribution of functionality.

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,35 @@
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class Game implements IGame {
-    private int numberOfPlayers;
-
+    private final int numberOfPlayers;
+    private List<Player> players;
+    private DrawingAndTrashPile drawingAndTrashPile;
+    private final SleepingQueens sleepingQueens;
     public Game(int numberOfPlayers) {
+        this.numberOfPlayers = numberOfPlayers;
+        List<Card> cards = new ArrayList<>();
 
+        // Add Kings
+        for(int i  = 0; i < 8; i++) {
+            cards.add(new Card(CardType.King, 1)); // TODO zistit value karty
+        }
+
+        // Add Knights
+        // TODO add knights and the rest
+
+        List<List<Card>> playerCards = new ArrayList<>();
+        for(int i = 0; i < numberOfPlayers; i++) {
+
+        }
+
+        sleepingQueens = new SleepingQueens();
+
+        for(int i = 0; i < numberOfPlayers; i++) {
+            Hand hand = new Hand()
+            Player player = new Player()
+        }
     }
 
     @Override
--- src/SleepingQueens.java
+++ src/SleepingQueens.java
@@ -2,7 +2,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class SleepingQueens extends QueenCollection {
-
+    public SleepingQueens() {
+        super();
+        for(int i = 0; i < 12; i++) {
+            queens.add(new Queen(1)); // TODO zistit ake body maju mat kralovne
+        }
+    }
     @Override
     public Map<Position, Queen> getQueens() {
         Map<Position, Queen> queenMap = new HashMap<>();

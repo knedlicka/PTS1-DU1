3cdbde52c5c704650c007970d0d59baa8ed52890
Nadiya Balanchuk <balanchuknadiy4@gmail.com>
2022-12-03 11:33:50 +0100

    Add observable to GameAdaptor
    * Fix logic in parsing input string
      in play function.

--- src/GameAdaptor.java
+++ src/GameAdaptor.java
@@ -1,18 +1,17 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GameAdaptor implements IGamePlayer{
     private final IGame game;
     private final Map<String, Integer> playerNames;
-    public GameAdaptor(Integer numberOfPlayers, List<String> playerNames){
+    private final GameObservable observable;
+    public GameAdaptor(Integer numberOfPlayers, List<String> playerNames, GameObservable observable) {
         IDealer dealer = new Dealer();
         game = new Game(numberOfPlayers, dealer.getDrawingPile(), dealer.getQueensSleeping());
         this.playerNames = new HashMap<>();
         for(int i = 0; i < playerNames.size(); i++){
             this.playerNames.put(playerNames.get(i), i);
         }
+        this.observable = observable;
     }
 
     @Override
@@ -24,12 +23,15 @@ public class GameAdaptor implements IGamePlayer{
         for(int i = 0; i < cards.length(); i++){
             if(cards.charAt(i) == 'h' && i + 1 < cards.length() && Character.isDigit(cards.charAt(i + 1))){
                 positions.add(new HandPosition(playerNames.get(player), Integer.parseInt(cards.charAt(i + 1) + "")));
+                i++;
             }
             else if(cards.charAt(i) == 's' && i + 1 < cards.length() && Character.isDigit(cards.charAt(i + 1))){
                 positions.add(new SleepingQueenPosition(Integer.parseInt(cards.charAt(i + 1) + "")));
+                i++;
             }
             else if(cards.charAt(i) == 'a' && i + 1 < cards.length() && Character.isDigit(cards.charAt(i + 1))){
                 positions.add(new AwokenQueenPosition(playerNames.get(player), Integer.parseInt(cards.charAt(i + 1) + "")));
+                i++;
             }
             else if(cards.charAt(i) == ' ') {
                 continue;
@@ -38,6 +40,9 @@ public class GameAdaptor implements IGamePlayer{
                 return "Invalid input";
             }
         }
-        return game.play(playerNames.get(player), positions).toString();
+
+        Optional<GameState> gameState = game.play(playerNames.get(player), positions);
+        gameState.ifPresent(observable::notifyAll);
+        return gameState.toString();
     }
 }
